2f3f7c55e3fb7cd4e78708890fbfff4fe2d17d98
qitas <qitas@qitas.cn>
2021-05-22 18:54:18 +0800

    fix

--- README.md
+++ README.md
@@ -9,26 +9,23 @@
 
 ## [简介](https://github.com/SoCXin/HC32M120/wiki)
 
-[HC32M120](https://www.hdsc.com.cn/Category84) 基于 Cortex®-M0+，集成最大 12 通道的 12bit 1MSPS ADC，2 个独立运算放大器（OPA），2 个独立电压比较器（CMP），1 个电机 Timer（Timer），1 个
-多功能 16bit Timer（TimerA）支持正交编码输入、输入捕捉、输出捕捉、PWM 输出，
-4 个多功能 16bit Timer（TimerB）支持输入捕捉、输出比较、PWM 输出，1 个 16bit Timer
-（Timer2）支持异步计数，1 个 16bit Timer（Timer0），1 个 I2C 通信接口，1 个 SPI 通
-信接口，3 个 USART 通信接口，其中 1 个 USART 支持 LIN 通信功能。
+[HC32M120](https://www.hdsc.com.cn/Category84) 基于 Cortex®-M0+，集成 12bit 1MSPS ADC，2 个独立运算放大器（OPA），2 个独立电压比较器（CMP），1 个电机 Timer（Timer），1 个
+多功能 16bit Timer（TimerA）支持正交编码输入、输入捕捉、输出捕捉、PWM 输出，4 个多功能 16bit Timer（TimerB）支持输入捕捉，3 个 USART 通信接口中 1 个支持 LIN 通信功能。
 
 [![sites](docs/HC32M120.png)](https://www.hdsc.com.cn/Category84-1492)
 
 #### 关键特性
 
-* 12 x 12bit 1MSPS ADC，支持各个Timer事件触发开始变换
+* 12bit 1MSPS ADC支持Timer事件触发
 * 2 x OPA，最大驱动电流30mA
 * 2 x CMP，支持窗口比较功能
 * 宽温宽电压，-40~105 ℃，2.7-5.5V
 
 #### 封装规格
 
-* LQFP32（7*7,0.8mm,H1.6mm）
-* LQFP48（7*7,0.5mm,H1.6mm）
-* HC32M140:TQFP48（7*7,0.5mm,H1.2mm）
+* LQFP32（7*7mm,P0.8mm,H1.6mm）
+* LQFP48（7*7mm,P0.5mm,H1.6mm）
+* [HC32M140](https://www.hdsc.com.cn/Category84-1494):TQFP48（7*7mm,P0.5mm,H1.2mm）
 
 ### [资源收录](https://github.com/SoCXin)
 
---
+++ src/template/hc32m120j6tb_template/example/source/main.c
@@ -0,0 +1,142 @@
+/**
+ *******************************************************************************
+ * @file  template/source/main.c
+ * @brief Main program template for the Device Driver Library.
+ @verbatim
+   Change Logs:
+   Date             Author          Notes
+   2019-06-28       Yangjp          First version
+ @endverbatim
+ *******************************************************************************
+ * Copyright (C) 2016, Huada Semiconductor Co., Ltd. All rights reserved.
+ *
+ * This software is owned and published by:
+ * Huada Semiconductor Co., Ltd. ("HDSC").
+ *
+ * BY DOWNLOADING, INSTALLING OR USING THIS SOFTWARE, YOU AGREE TO BE BOUND
+ * BY ALL THE TERMS AND CONDITIONS OF THIS AGREEMENT.
+ *
+ * This software contains source code for use with HDSC
+ * components. This software is licensed by HDSC to be adapted only
+ * for use in systems utilizing HDSC components. HDSC shall not be
+ * responsible for misuse or illegal use of this software for devices not
+ * supported herein. HDSC is providing this software "AS IS" and will
+ * not be responsible for issues arising from incorrect user implementation
+ * of the software.
+ *
+ * Disclaimer:
+ * HDSC MAKES NO WARRANTY, EXPRESS OR IMPLIED, ARISING BY LAW OR OTHERWISE,
+ * REGARDING THE SOFTWARE (INCLUDING ANY ACCOMPANYING WRITTEN MATERIALS),
+ * ITS PERFORMANCE OR SUITABILITY FOR YOUR INTENDED USE, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, THE IMPLIED
+ * WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE OR USE, AND THE IMPLIED
+ * WARRANTY OF NONINFRINGEMENT.
+ * HDSC SHALL HAVE NO LIABILITY (WHETHER IN CONTRACT, WARRANTY, TORT,
+ * NEGLIGENCE OR OTHERWISE) FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+ * LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION,
+ * LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING FROM USE OR
+ * INABILITY TO USE THE SOFTWARE, INCLUDING, WITHOUT LIMITATION, ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OR LOSS OF DATA,
+ * SAVINGS OR PROFITS,
+ * EVEN IF Disclaimer HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * YOU ASSUME ALL RESPONSIBILITIES FOR SELECTION OF THE SOFTWARE TO ACHIEVE YOUR
+ * INTENDED RESULTS, AND FOR THE INSTALLATION OF, USE OF, AND RESULTS OBTAINED
+ * FROM, THE SOFTWARE.
+ *
+ * This software may be replicated in part or whole for the licensed use,
+ * with the restriction that this Disclaimer and Copyright notice must be
+ * included with each copy of this software, whether used in part or whole,
+ * at all times.
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ * Include files
+ ******************************************************************************/
+#include "hc32_ddl.h"
+
+/**
+ * @addtogroup HC32M120_DDL_Examples
+ * @{
+ */
+
+/**
+ * @addtogroup Templates
+ * @{
+ */
+
+/*******************************************************************************
+ * Local type definitions ('typedef')
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local pre-processor symbols/macros ('#define')
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Global variable definitions (declared in header file with 'extern')
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local function prototypes ('static')
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local variable definitions ('static')
+ ******************************************************************************/
+/**
+ * @brief ICG parameters configuration
+ */
+  /* The ICG area is filled with F by default, HRC = 24MHZ,
+    Please modify this value as required */
+#if defined (__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
+const uint32_t u32ICG[] __attribute__((section(".ARM.__at_0xC0"))) =
+#elif defined (__GNUC__) && !defined (__CC_ARM)
+const uint32_t u32ICG[] __attribute__((section(".icg_sec"))) =
+#elif defined (__CC_ARM)
+const uint32_t u32ICG[] __attribute__((at(0xC0))) =
+#elif defined (__ICCARM__)
+#pragma location = 0xC0
+__root static const uint32_t u32ICG[] =
+#else
+#error "unsupported compiler!!"
+#endif
+{
+    /* ICG 0~ 3 */
+    0xFFF9FFFFUL,
+    0xFFFFFFFFUL,
+    0xFFFFFFFFUL,
+    0xFFFFFFFFUL,
+    /* ICG 4~ 6 */
+    0xFFFFFFFFUL,
+    0xFFFFFFFFUL,
+    0xFFFFFFFFUL,
+};
+/*******************************************************************************
+ * Function implementation - global ('extern') and local ('static')
+ ******************************************************************************/
+
+/**
+ * @brief  Main function of template project
+ * @param  None
+ * @retval int32_t return value, if needed
+ */
+int32_t main(void)
+{
+    /* Add your code here */
+    while (1)
+    {
+    }
+}
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/*******************************************************************************
+ * EOF (not truncated)
+ ******************************************************************************/
